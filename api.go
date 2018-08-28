b9e9da5d041b70ab2520f9b046319151ac4ec2da
Antony Shchukin <szwtdy@gmail.com>
2018-08-28 12:59:09 +0300

    refactor: rename

--- api.go
+++ api.go
@@ -20,25 +20,25 @@ type Tokener interface {
 	TotalSupply() (*big.Int, error)
 }
 
-type API struct {
+type Token struct {
 	client   *ethclient.Client
 	gasPrice *big.Int
 
 	tokenContract *StandardToken
 }
 
-func NewAPI(ethEndpoint string, gasPrice *big.Int, tokenContractAddress string) (*API, error) {
+func NewToken(ethEndpoint string, gasPrice *big.Int, contractAddress string) (*Token, error) {
 	client, err := ethclient.Dial(ethEndpoint)
 	if err != nil {
 		return nil, err
 	}
 
-	tokenContract, err := NewStandardToken(common.HexToAddress(tokenContractAddress), client)
+	tokenContract, err := NewStandardToken(common.HexToAddress(contractAddress), client)
 	if err != nil {
 		return nil, err
 	}
 
-	bch := &API{
+	bch := &Token{
 		client:        client,
 		gasPrice:      gasPrice,
 		tokenContract: tokenContract,
@@ -46,51 +46,51 @@ func NewAPI(ethEndpoint string, gasPrice *big.Int, tokenContractAddress string) 
 	return bch, nil
 }
 
-func (api *API) BalanceOf(ctx context.Context, address string) (*big.Int, error) {
-	balance, err := api.tokenContract.BalanceOf(getCallOpts(ctx), common.HexToAddress(address))
+func (t *Token) BalanceOf(ctx context.Context, address string) (*big.Int, error) {
+	balance, err := t.tokenContract.BalanceOf(getCallOpts(ctx), common.HexToAddress(address))
 	if err != nil {
 		return nil, err
 	}
 	return balance, nil
 }
 
-func (api *API) AllowanceOf(ctx context.Context, from string, to string) (*big.Int, error) {
-	allowance, err := api.tokenContract.Allowance(getCallOpts(ctx), common.HexToAddress(from), common.HexToAddress(to))
+func (t *Token) AllowanceOf(ctx context.Context, from string, to string) (*big.Int, error) {
+	allowance, err := t.tokenContract.Allowance(getCallOpts(ctx), common.HexToAddress(from), common.HexToAddress(to))
 	if err != nil {
 		return nil, err
 	}
 	return allowance, nil
 }
 
-func (api *API) Approve(ctx context.Context, key *ecdsa.PrivateKey, to string, amount *big.Int) (*types.Transaction, error) {
+func (t *Token) Approve(ctx context.Context, key *ecdsa.PrivateKey, to string, amount *big.Int) (*types.Transaction, error) {
 	opts := getTxOpts(key, 48000)
-	tx, err := api.tokenContract.Approve(opts, common.HexToAddress(to), amount)
+	tx, err := t.tokenContract.Approve(opts, common.HexToAddress(to), amount)
 	if err != nil {
 		return nil, err
 	}
 	return tx, err
 }
 
-func (api *API) Transfer(ctx context.Context, key *ecdsa.PrivateKey, to string, amount *big.Int) (*types.Transaction, error) {
+func (t *Token) Transfer(ctx context.Context, key *ecdsa.PrivateKey, to string, amount *big.Int) (*types.Transaction, error) {
 	opts := getTxOpts(key, 40000)
-	tx, err := api.tokenContract.Transfer(opts, common.HexToAddress(to), amount)
+	tx, err := t.tokenContract.Transfer(opts, common.HexToAddress(to), amount)
 	if err != nil {
 		return nil, err
 	}
 	return tx, err
 }
 
-func (api *API) TransferFrom(ctx context.Context, key *ecdsa.PrivateKey, from string, to string, amount *big.Int) (*types.Transaction, error) {
+func (t *Token) TransferFrom(ctx context.Context, key *ecdsa.PrivateKey, from string, to string, amount *big.Int) (*types.Transaction, error) {
 	opts := getTxOpts(key, 50000)
-	tx, err := api.tokenContract.TransferFrom(opts, common.HexToAddress(from), common.HexToAddress(to), amount)
+	tx, err := t.tokenContract.TransferFrom(opts, common.HexToAddress(from), common.HexToAddress(to), amount)
 	if err != nil {
 		return nil, err
 	}
 	return tx, err
 }
 
-func (api *API) TotalSupply(ctx context.Context) (*big.Int, error) {
-	totalSupply, err := api.tokenContract.TotalSupply(getCallOpts(ctx))
+func (t *Token) TotalSupply(ctx context.Context) (*big.Int, error) {
+	totalSupply, err := t.tokenContract.TotalSupply(getCallOpts(ctx))
 	if err != nil {
 		return nil, err
 	}
